f47e321571fda2abd5169d832d7daa183e7c7595
Tam <tamborine@Tams-MacBook-Air.local>
2016-09-30 14:33:26 +0100

    refactoring

--- src/interface.js
+++ src/interface.js
@@ -28,52 +28,7 @@ blobListen('#document').ready(function(){
 
   request.send();
 
-
-//  Helper functions follow
-
-  function createLiElements(){
-    item = document.createElement('li');
-    headline = document.createElement('h1');
-    summary = document.createElement('p');
-    image = document.createElement('img');
-    link = document.createElement('a');
-    link.appendChild(document.createTextNode("see full article.."));
-  }
-
-
-  function addLi(i, story) {
-    // creates <li></li>
-    createLiElements();
-
-    headline.appendChild(document.createTextNode(story.headline));
-    summary.appendChild(document.createTextNode(story.summarised));
-    image.setAttribute("src", story.imgUrl);
-    console.log(story.webUrl);
-    link.setAttribute("href", story.webUrl);
-    // creates <li id="headline-0"></li>
-    item.setAttribute("id", "headline-" + i);
-    // append the actual string to the item (which is the li)
-    addToItem(headline,image,summary,link);
-    console.log(item);
-    return item;
-  }
-
-
-  function renderStories(stories){
-    stories.forEach(function(story){
-      var i = stories.indexOf(story);
-      document.getElementById('storyList').appendChild(addLi(i, story));
-
-    })
-  }
-
-  function addToItem(headline,image,summary,link){
-    item.appendChild(headline);
-    item.appendChild(image);
-    item.appendChild(summary);
-    summary.appendChild(link);
-  }
-
+//n/a until summarised works
 
   function createCloseBtn() {
     var close = document.createElement("input");
@@ -82,19 +37,10 @@ blobListen('#document').ready(function(){
     close.setAttribute("value", "close");
     return close;
   }
-
-  function addLiListener(item, index) {
-    item.addEventListener('click', function(){
-      blobListen('container').hide();
-      blobListen('article-'+index).show();
-    }, false);
-  }
-
-  function addCloseListener(close, index) {
-    close.addEventListener('click', function(){
-      blobListen('container').show();
-      blobListen('article-'+index).hide();
-    }, false);
-  }
+function ListenToShowArticle(i) {
+  BlobListen("headline-" + i).click(function(){
+    //make API call to Aylien
+  });
+}
 
 });
