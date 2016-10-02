d4340b570820d4f94e90f4915d6310825f651249
Abigail <abigail.mcphillips@gmail.com>
2016-10-02 14:53:53 +0100

    notes_on_functionality

--- src/interface.js
+++ src/interface.js
@@ -1,21 +1,22 @@
 
 blobListen('#document').ready(function(){
 
-
   var story = new Story();
-
   var request = new XMLHttpRequest();
   var apiKey = "api-key=59001b87-63d3-4d83-aa21-ed20cfdbd037";
   var date = new Date();
-
   today = date.getFullYear()+"-"+ (date.getMonth() + 1)+ "-" + date.getDate();
 
+  //makes a request to the guardian API with today's date and using our API key
   request.open('GET', 'http://content.guardianapis.com/search?from-date=' + today + '&to-date='+ today +'&order-by=newest&show-fields=all&page-size=5&' + apiKey , true);
 
+  //once the request has been sent, run this function
   request.onload = function() {
     if (request.status >= 200 && request.status < 400) {
-      // Success!
+      // if success, get response text from server and parse string to JSON
       var data = JSON.parse(request.responseText);
+      //data.response.results is array of five 'news story' objects requested
+      //responseParse returns array of stories to work with
       stories = responseParse(data.response.results);
       renderStories(stories);
       children = listChildren();
--- src/interfaceHelpers.js
+++ src/interfaceHelpers.js
@@ -1,39 +1,54 @@
 function responseParse(responseDotResults){
       stories = [];
+      //iterates through array of stories
       responseDotResults.forEach(function(article){
         var a = article;
+        //fields contains all story info
         var af = article.fields;
+        //create new (empty) story from model
         story = new Story();
+        //set attributes
         story.headline = af.headline;
         story.summarised = "something function that deals with Aylien";
         story.imgUrl = af.thumbnail;
         story.webUrl = a.webUrl;
+        //save into array of stories to be published
         stories.push(story);
       });
       return stories;
 }
 
+//adds stories to main page
 function renderStories(stories){
+  //iterates through array of (model) stories
   stories.forEach(function(story){
+    //sets an index according to array position
     var i = stories.indexOf(story);
+    //adds an Li in Ol for each story (with its index)
     document.getElementById('storyList').appendChild(addLi(i, story));
   });
 }
 
+//adds detailed html to each story
 function addLi(i, story) {
+  //creates html elements needed for each Li
   createLiElements();
+  //inserts headline text into h1
   headline.appendChild(document.createTextNode(story.headline));
   summary.appendChild(document.createTextNode(story.summarised));
+  //sets img src to be imgurl
   image.setAttribute("src", story.imgUrl);
+  //sets link href to be weburl
   link.setAttribute("href", story.webUrl);
+  //sets individual Li ids
   item.setAttribute("id", "headline-" + i);
   summary.setAttribute("id", "summary-" + i);
-  summary.setAttribute('style', 'display:none');
   link.setAttribute('id', 'link-' + i);
+  //hides summary on load up
+  summary.setAttribute('style', 'display:none');
   addToItem(headline,image,summary,link);
   return item;
 }
-
 
 function createLiElements(){
   item = document.createElement('li');
@@ -44,7 +59,8 @@ function createLiElements(){
   link.appendChild(document.createTextNode("see full article.."));
 }
 
-
+//attaches each html item to Li
+//attaches link to summary
 function addToItem(headline,image,summary,link){
   item.appendChild(headline);
   item.appendChild(image);
